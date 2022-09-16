1f0f71fc024a114209981e935c9fc0d74f215639
coderajesh14 <110840029+coderajesh14@users.noreply.github.com>
2022-09-16 17:11:04 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/src/GameMain.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class GameMain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Tictactoe game = new Tictactoe();
+        String player = "X";
+
+        do {
+            System.out.println(game.printBoard());
+            System.out.println("enter row for " + player + " or -1 to exit: ");
+            int row = input.nextInt();
+            if(row == -1)
+                break;
+            System.out.println("enter column for " + player + ": ");
+            int column = input.nextInt();
+            game.setPlay(row,  column, player);
+            if(game.isGameOver()) {
+                System.out.println(game.printBoard() + "\n" + player +" wins!");
+                break;
+            }
+            if(player == "X") {
+                player = "O";
+            }
+            else {
+                player = "X";
+            }
+        }while(true);
+        System.out.println("Goodbye!..");
+
+    }
+
+}
